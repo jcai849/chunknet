97dbe57547087aa36ef156f34e771bfc205a141d
Jason Cairns <jason.cairns@aucklanduni.ac.nz>
2021-10-17 15:08:48 +1300

    allow for context initiation in Endpoint

--- R/communication.R
+++ R/communication.R
@@ -1,13 +1,13 @@
 Endpoint <- function(type, connection) {
-	function(at) {
+	function(at, context) {
 		stopifnot(is.Location(at))
-		context <- init.context()
-		endpoint <- init.socket(context, type)
+		initiated_context <- if (missing(context)) init.context() else context
+		endpoint <- init.socket(initiated_context, type)
 		class(endpoint) <- c(switch(type,
-					    ZMQ_REP="Replier",
-					    ZMQ_REQ="Requester",
-					    ZMQ_PUB="Publisher",
-					    ZMQ_SUB="Subscriber"),
+					    ZMQ_REP=c("Replier", "Listener"),
+					    ZMQ_REQ=c("Requester", "Speaker"),
+					    ZMQ_PUB=c("Publisher", "Speaker"),
+					    ZMQ_SUB=c("Subscriber", "Listener")),
 				     "Endpoint",
 				     class(endpoint))
 		connection(endpoint, as.character(at))
@@ -15,25 +15,36 @@ Endpoint <- function(type, connection) {
 	}
 }
 is.Endpoint <- function(x) inherits(x, "Endpoint")
-Replier <- Endpoint("ZMQ_REP", bind.socket)
-is.Replier <- function(x) inherits(x, "Replier")
-Requester <- Endpoint("ZMQ_REQ", connect.socket)
-is.Requester <- function(x) inherits(x, "Requester")
-Publisher <- Endpoint("ZMQ_PUB", bind.socket)
-is.Publisher <- function(x) inherits(x, "Publisher")
-Subscriber <- function(at) {
-	socket <- Endpoint("ZMQ_SUB", connect.socket)(at)
-	subscribe(socket, '')
-	socket
+format.Endpoint <- function(x, ...) format(as.Location(x))
+print.Endpoint <- function(x, ...) cat("Endpoint: ", format(x), "\n")
+
+is.Listener <- function(x) inherits(x, "Listener")
+print.Listener <- function(x, ...) {
+	cat("Listener with last binding at: ")
+	NextMethod()
 }
+is.Speaker <- function(x, ...) inherits(x, "Speaker")
+print.Speaker <- function(x, ...) {
+	cat("Speaker with last connection at: ")
+	NextMethod()
+}
+
+maybe_connect <- function(endpoint, at)
+	if (!missing(at)) connect.socket(endpoint, at)
+Replier <- Endpoint("ZMQ_REP", bind.socket)
+Requester <- Endpoint("ZMQ_REQ", maybe_connect)
+Publisher <- Endpoint("ZMQ_PUB", bind.socket)
+Subscriber <- Endpoint("ZMQ_SUB",
+		       function(endpoint, at) {
+			       maybe_connect(endpoint, at)
+			       subscribe(endpoint, '')
+		       })
+is.Publisher <- function(x) inherits(x, "Publisher")
+is.Requester <- function(x) inherits(x, "Requester")
+is.Replier <- function(x) inherits(x, "Replier")
 is.Subscriber <- function(x) inherits(x, "Subscriber")
 
-format.Endpoint <- function(x, ...) format(as.Location(x))
-print.Replier <- function(x, ...)
-	cat("Replier with last binding at location: ", format(x), "\n")
-print.Requester <- function(x, ...)
-	cat("Requester with last connection at location: ", format(x), "\n")
-print.Publisher <- function(x, ...)
-	cat("Publisher with last binding at location: ", format(x), "\n")
-print.Subscriber <- function(x, ...)
-	cat("Subscriber with last connection at location: ", format(x), "\n")
+print.Replier <- function(x, ...) { cat("Replier: "); NextMethod() }
+print.Requester <- function(x, ...) { cat("Requester: "); NextMethod() }
+print.Publisher <- function(x, ...) { cat("Publisher: "); NextMethod() }
+print.Subscriber <- function(x, ...) { cat("Subscriber: "); NextMethod() }
