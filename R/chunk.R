ed4216bb0ac76616e61d6807ea66b17eb378f17b
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-05-18 09:39:03 +1200

    move chunk code to new file

---
+++ R/chunk.R
@@ -0,0 +1,29 @@
+Chunk <- function(href, generator_href) {
+       stopifnot(is.character(href),
+                 is.character(generator_href))
+       chunk <- structure(new.env(parent=emptyenv(), size=2L), class="Chunk")
+       chunk$href <- href
+       chunk$generator_href <- generator_href
+       chunk
+}
+
+TransientChunk <- function(href, generator_href) {
+        chunk <- Chunk(href, generator_href)
+        class(chunk) <- c("TransientChunk", class(chunk))
+        chunk
+}
+
+StableChunk <- function(href, generator_href) {
+        chunk <- Chunk(href, generator_href)
+        class(chunk) <- c("StableChunk", class(chunk))
+        reg.finalizer(chunk, delete)
+        chunk
+}
+
+delete <- function(chunk) {
+        locations <- get_location(chunk)
+        for (i in seq(NROW(locations))) {
+                event_external_push(paste0("DELETE /data/", chunk$href), NULL, locations[i, "address"], locations[i, "port"])
+        }
+        event_external_push(paste0("DELETE /data/", chunk$href), NULL, LOCATOR()$address, LOCATOR()$port)
+}
--- R/client.R
+++ R/client.R
@@ -1,33 +1,3 @@
-Chunk <- function(href, generator_href) {
-       stopifnot(is.character(href),
-                 is.character(generator_href))
-       chunk <- structure(new.env(parent=emptyenv(), size=2L), class="Chunk")
-       chunk$href <- href
-       chunk$generator_href <- generator_href
-       chunk
-}
-
-TransientChunk <- function(href, generator_href) {
-	chunk <- Chunk(href, generator_href)
-	class(chunk) <- c("TransientChunk", class(chunk))
-	chunk
-}
-
-StableChunk <- function(href, generator_href) {
-	chunk <- Chunk(href, generator_href)
-	class(chunk) <- c("StableChunk", class(chunk))
-	reg.finalizer(chunk, delete)
-	chunk
-}
-
-delete <- function(chunk) {
-        locations <- get_location(chunk)
-        for (i in seq(NROW(locations))) {
-                event_external_push(paste0("DELETE /data/", chunk$href), NULL, locations[i, "address"], locations[i, "port"])
-        }
-        event_external_push(paste0("DELETE /data/", chunk$href), NULL, LOCATOR()$address, LOCATOR()$port)
-}
-
 post_location <- function(href, location) {
 	log("Sending location of %s to Locator", href)
 	event_external_push(paste0("POST /data/", href), location, LOCATOR()$address, LOCATOR()$port)
