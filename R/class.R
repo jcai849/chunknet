e86a3d8e398f257e365f2700cd36c488f8b24239
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-05-18 10:12:50 +1200

    contain all classes in single file

--- R/class.R
+++ R/class.R
@@ -27,3 +27,13 @@ delete <- function(chunk) {
         }
         event_external_push(paste0("DELETE /data/", chunk$href), NULL, LOCATOR()$address, LOCATOR()$port)
 }
+
+Computation <- function(procedure, arguments, alignments, href, output_href) {
+	comp <- structure(new.env(parent=emptyenv(), size=5L), class="Computation")
+	comp$procedure <- procedure
+	comp$arguments <- arguments
+	comp$alignments <- alignments
+	comp$href <- href
+	comp$output_href <- output_href
+	comp
+}
