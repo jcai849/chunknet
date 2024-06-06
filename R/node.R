f6078f07f42020fa56a8f1eaf386f899213427dd
Jason Cairns <jason.cairns@auckland.ac.nz>
2024-06-06 14:08:23 +1200

    rename msg to event in node function

--- R/node.R
+++ R/node.R
@@ -15,7 +15,7 @@ node <- function(init_function) {
                 init_function(...)
                 repeat {
                         event <- orcv::receive(keep_conn=TRUE)
-			log("Opening message with header \"%s\"\n", orcv::header(msg))
+			log("Opening message with header \"%s\"\n", orcv::header(event))
                         handle(event)
                         log("...DONE")
                 }
